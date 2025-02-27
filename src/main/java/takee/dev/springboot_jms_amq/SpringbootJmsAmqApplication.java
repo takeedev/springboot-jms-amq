78a02fe509121c286913378e006fbcd3eacc8ebb
taknaja <peerapolkoeram@gmail.com>
2025-02-27 22:45:53 +0700

    init project

---
+++ src/main/java/takee/dev/springboot_jms_amq/SpringbootJmsAmqApplication.java
@@ -0,0 +1,13 @@
+package takee.dev.springboot_jms_amq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJmsAmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJmsAmqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/takee/dev/springboot_jms_amq/SpringbootJmsAmqApplicationTests.java
@@ -0,0 +1,13 @@
+package takee.dev.springboot_jms_amq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJmsAmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
